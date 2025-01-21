4cb82a092e056c4ac4692c0a99ec9ddb55444c18
vale <valeriano@gmail.com>
2025-01-21 18:49:02 +0100

    comentario +++

--- msalumnosprofe/src/main/java/edu/kyndryl/academy/msalumnosprofe/MsalumnosprofeApplication.java
+++ msalumnosprofe/src/main/java/edu/kyndryl/academy/msalumnosprofe/MsalumnosprofeApplication.java
@@ -3,9 +3,10 @@ package edu.kyndryl.academy.msalumnosprofe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication//clase de configuracion
+@SpringBootApplication//clase de configuracion pendiente comentar
 public class MsalumnosprofeApplication {
 
+	//TODO comentar mejoras
 	public static void main(String[] args) {
 		SpringApplication.run(MsalumnosprofeApplication.class, args);
 	}

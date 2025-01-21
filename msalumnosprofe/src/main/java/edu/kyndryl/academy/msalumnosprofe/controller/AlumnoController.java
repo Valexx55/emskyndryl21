31a5aed3981a7b18027cf3fa7a4fef9766d55167
vale <valeriano@gmail.com>
2025-01-21 15:02:02 +0100

    logback config

--- msalumnosprofe/src/main/java/edu/kyndryl/academy/msalumnosprofe/controller/AlumnoController.java
+++ msalumnosprofe/src/main/java/edu/kyndryl/academy/msalumnosprofe/controller/AlumnoController.java
@@ -1,5 +1,7 @@
 package edu.kyndryl.academy.msalumnosprofe.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,7 +30,9 @@ import edu.kyndryl.academy.msalumnosprofe.model.Alumno;
 @RestController//le estoy diciendo que trabajo con JSON - de modo que si yo devuelvo un alumno , Spring automáticamente lo convierte a JSON
 public class AlumnoController {
 	
-	//TODO CREAR PRIMER SERVICIO WEB ALUMNO-TEST
+	
+	
+	Logger logger = LoggerFactory.getLogger(AlumnoController.class);//dame un log para esta clase
 	
 	//yo a Spring le digo oye: si recibes un GET localhost:8081/obtener-alumno-test
 	//invocas a este método
@@ -36,7 +40,11 @@ public class AlumnoController {
 	public Alumno obtenerAlumnoTest () {
 		Alumno alumno = null;
 		
+			logger.debug("Entrando en obtenerAlumnoTest");
 			alumno = new Alumno("Laura", 60, "Gómez", "laura@kyndryl.com");
+			logger.debug("Alumno " + alumno);
+			logger.debug("Final obtenerAlumnoTest");
+			//TODO COMENTAR TOSTRING EN ENTITADES Y BEANS LOG
 		
 		return alumno;//SERIALIZA Objeto de Java - A String JSON
 	}

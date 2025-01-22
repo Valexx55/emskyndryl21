f16cfb61c5484cdf360224bf33646fe78403515a
vale <valeriano@gmail.com>
2025-01-22 10:28:33 +0100

    crud repo

--- msalumnosprofe/src/main/java/edu/kyndryl/academy/msalumnosprofe/repository/AlumnoRepository.java
+++ msalumnosprofe/src/main/java/edu/kyndryl/academy/msalumnosprofe/repository/AlumnoRepository.java
@@ -1,11 +1,14 @@
 package edu.kyndryl.academy.msalumnosprofe.repository;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import edu.kyndryl.academy.msalumnosprofe.model.Alumno;
 
 /**
  * en esta clase, operamos con la base de datos
  */
 @Repository
-public interface AlumnoRepository {
+public interface AlumnoRepository extends CrudRepository<Alumno, Long> {
 
 }
--- msalumnosprofe/src/main/java/edu/kyndryl/academy/msalumnosprofe/service/AlumnoServiceImp.java
+++ msalumnosprofe/src/main/java/edu/kyndryl/academy/msalumnosprofe/service/AlumnoServiceImp.java
@@ -1,12 +1,46 @@
 package edu.kyndryl.academy.msalumnosprofe.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+
+import edu.kyndryl.academy.msalumnosprofe.model.Alumno;
 
 /**
  * En esta clase, contiene la lógica de negocio
  * la implementación de las operaciones
  */
 @Service
-public class AlumnoServiceImp {
+public class AlumnoServiceImp implements AlumnoService {
+
+	@Override
+	public Alumno alta(Alumno alumno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void borrarPorId(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Optional<Alumno> modificarAlumnoPorId(Alumno alumno, Long id) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public Optional<Alumno> consultarPorId(Long id) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public Iterable<Alumno> consultarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }

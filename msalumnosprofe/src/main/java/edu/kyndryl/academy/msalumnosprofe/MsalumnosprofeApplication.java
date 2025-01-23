208de4c478b3db636135f3672dac6d9eeda0ae56
vale <valeriano@gmail.com>
2025-01-23 12:37:07 +0100

    EUREKA cliente y servidor conectados

--- msalumnosprofe/src/main/java/edu/kyndryl/academy/msalumnosprofe/MsalumnosprofeApplication.java
+++ msalumnosprofe/src/main/java/edu/kyndryl/academy/msalumnosprofe/MsalumnosprofeApplication.java
@@ -2,8 +2,11 @@ package edu.kyndryl.academy.msalumnosprofe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication//clase de configuracion pendiente comentar
+@EnableDiscoveryClient //activamos el cliente eureka
 public class MsalumnosprofeApplication {
 
 	//TODO comentar mejoras

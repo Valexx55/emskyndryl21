00e3a9316963b0d4860c3a4641d558759bb3a751
Val <valexx55@gmail.com>
2025-03-21 14:32:48 +0100

    librerias jwt y UserDetailImp

--- msalumnosprofe/src/main/java/edu/kyndryl/academy/msalumnosprofe/security/model/UserDetailImpl.java
+++ msalumnosprofe/src/main/java/edu/kyndryl/academy/msalumnosprofe/security/model/UserDetailImpl.java
@@ -1,5 +1,81 @@
 package edu.kyndryl.academy.msalumnosprofe.security.model;
 
-public class UserDetailImpl {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+//esta es la clase que realmente usará Spring para acceder a las credenciales de un usuario
+
+public class UserDetailImpl implements UserDetails {
+	
+	private Usuario usuario;
+	
+	public UserDetailImpl(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> authorities = new ArrayList<>();
+			
+			//hacemos una equivalencia entre Rol Y Autoridad
+			this.usuario.getRoles().forEach(
+					rol -> {
+						authorities.add(new SimpleGrantedAuthority(rol.getTipRol().toString()));
+					}
+					);
+		
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return usuario.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return usuario.getEmail();
+	}
+	
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return (!this.usuario.getBloqueada());
+	}
+	
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return this.usuario.getActivada();
+	}
+	
+	public String getNombre ()
+	{
+		return this.usuario.getNombre();
+	}
+	
+	
+	
 
 }
